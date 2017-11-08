064e5e9360e54c9591ee200f600f740e696ff138
李楠 <948094798@qq.com>
2017-11-08 21:03:01 +0800

    changes log

---
+++ 实验四/实验四/src/Main.java
@@ -0,0 +1,46 @@
+
+import java.util.TreeSet;
+import java.util.SortedSet;
+public class Main {
+	public static void main(String args[])throws Exception{
+		wrFile wr=new wrFile();
+		wr.createFile();
+		SortedSet<Task> ts=new TreeSet<>();
+		
+		wr.readFile(ts);
+		FSCS fs=new FSCS();
+		fs.fscsSingle(ts);
+		
+		System.out.println("先到先服务：\n单队列");
+		System.out.println(ts);
+
+		System.out.println();
+		
+		wrFile wr1=new wrFile();
+		SortedSet<Task> ts1=new TreeSet<>();
+		
+		wr1.readFile(ts1);
+		fs.fscsDouble(ts1);
+		System.out.println("双队列");
+		fs.printDouble(ts1);
+		
+		System.out.println();
+		wrFile wr2=new wrFile();
+		SortedSet<Task> ts2=new TreeSet<>();
+		SJF sjf=new SJF();
+		wr2.readFile(ts2);
+		sjf.sjfSingle(ts2);
+		System.out.println("短作业优先：\n单队列");
+		System.out.println(ts2);
+		
+		System.out.println();
+		wrFile wr3=new wrFile();
+		SortedSet<Task> ts3=new TreeSet<>();
+		
+		wr3.readFile(ts3);
+		sjf.sjfDouble(ts3);
+		System.out.println("双队列");
+		sjf.printDouble(ts3);
+		
+	}
+}
---
+++ 实验四/实验四/src/Task.java
@@ -0,0 +1,75 @@
+
+public class Task implements Comparable<Task>{
+	private int taskID;
+	private int arrivalTime;
+	private int serviceTime;
+	private int startingTime;
+	private int finishingTime;
+	private int turnAroundTime;
+	private double weightTurnAround;
+	private int tag;
+	Task(){
+	}
+	Task(int id,int arTime,int serTime){
+		this.taskID=id;
+		this.arrivalTime=arTime;
+		this.serviceTime=serTime;
+		this.startingTime=0;
+		this.finishingTime=0;
+		this.turnAroundTime=0;
+		this.weightTurnAround=0;
+		this.tag=0;
+	}
+	public int getID(){
+		return taskID;
+	}
+	public int getArrTime()
+	{
+		return arrivalTime;
+	}
+	public int getSerTime(){
+		return serviceTime;
+	}
+	public int getStartTime(){
+		return startingTime;
+	}
+	
+	public int getFinTime(){
+		return finishingTime;
+	}
+	public int getTurnAround(){
+		return turnAroundTime;
+	}
+	public double getWeight()
+	{
+		return weightTurnAround;
+	}
+	public int getTag(){
+		return tag;
+	}
+	public void setStart(int start){
+		this.startingTime=start;
+	}
+	public void setFinish(int finish){
+		this.finishingTime=finish;
+	}
+	public void setTurnAround(int turnAround)
+	{
+		this.turnAroundTime=turnAround;
+	}
+	public void setWeight(double weightTurnA){
+		this.weightTurnAround=weightTurnA;
+	}
+	public void setTag(int tag)
+	{
+		this.tag=tag;
+	}
+	public int compareTo(Task t)
+	{
+		return Integer.compare(taskID, t.taskID);
+	}
+	public String toString()
+	{
+		return taskID+"  "+arrivalTime+"  "+serviceTime+"  "+startingTime+"  "+finishingTime+"  "+turnAroundTime+"  "+weightTurnAround+"¡¡"+tag+"\n";
+	}
+}
